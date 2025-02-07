4e4081af07dc02fb847c520041826417a3284f89
Danial Rhz <danial.rahimzadeh@umain.com>
2025-02-07 17:47:34 +0100

    solve conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './video/video.module';
+import { VideoController } from './videoProcessing/video.controller';
+import { VideoProcessingService } from './videoProcessing/video-processing.service';
 
 @Module({
   imports: [VideoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, VideoController],
+  providers: [AppService, VideoProcessingService],
 })
 export class AppModule {}
