62e5b4ab6f2eda51801146834e2445c12d47c902
Danial Rhz <danial.rahimzadeh@umain.com>
2025-02-08 12:20:03 +0100

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 
-dotenv.config();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Enable CORS
   app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'client-id'],
+    origin: 'https://nestream-web.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   app.useWebSocketAdapter(new IoAdapter(app));
-
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
